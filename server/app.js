2e4c0d8e3c95069ec21fe75c69859f4adf0d73ef
JSisques <javierplazasisques@gmail.com>
2021-11-17 21:40:02 +0100

    Updated

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-const hostname = '0.0.0.0';
-const port = 3000;
+// Servimos archivos estáticos en la carpeta public
+app.use(express.static('public'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
-});
+// Iniciamos una conexión con Socket.IO y con un mensaje
+io.on('connection', function (socket) {
+  console.log('Nuevo cliente conectado')
+  socket.emit('mensaje', 'Bienvenido!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-});
+// Enviamos el mensaje del socket con un setInterval
+setInterval(function () {
+  io.emit('mensaje', 'Hola, desde un Socket.IO de Platzi')
+}, 3000)
+
+// Iniciamos el servidor en el puerto 8080
+server.listen(8080, function () {
+  console.log('Servidor iniciado en http://localhost:8080')
+})
