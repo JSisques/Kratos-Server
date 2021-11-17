c54046796807d016f3ce4d7d88c3853bab12e6d3
JSisques <javierplazasisques@gmail.com>
2021-11-17 20:59:04 +0100

    Updated

--- server/app.js
+++ server/app.js
@@ -3,6 +3,4 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('3000', function() {
-  console.log('Servidor web escuchando en el puerto 3000');
-});
+app.listen('3000', '192.168.1.237');
